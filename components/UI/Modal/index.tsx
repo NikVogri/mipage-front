59f0ec3d7f65e4b9703a5cdf9e12ab0576bb0837
NikVogri <vogrinec.nik@gmail.com>
2022-01-14 20:49:12 +0100

    feat: base modal component

---
+++ components/UI/Modal/index.tsx
@@ -0,0 +1,58 @@
+import BaseModal from "react-modal";
+import { IoMdClose } from "react-icons/io";
+
+import styles from "./BaseModal.module.scss";
+
+interface ModalProps {
+	isOpen: boolean;
+	setIsOpen: (isOpen: boolean) => void;
+	children: React.ReactNode;
+	contentLabel: string;
+}
+
+interface FooterModalProps {
+	children?: React.ReactNode;
+}
+
+interface HeadModalProps {
+	title: string;
+	children?: React.ReactNode;
+	closeModal?: () => void;
+}
+
+const Modal = ({ isOpen, setIsOpen, children, contentLabel }: ModalProps) => {
+	return (
+		<BaseModal
+			ariaHideApp={false}
+			isOpen={isOpen}
+			contentLabel={contentLabel}
+			className={styles.modal}
+			overlayClassName={styles.modal__background}
+			onRequestClose={() => setIsOpen(false)}
+		>
+			<div className={styles.modal__body}>{children}</div>
+		</BaseModal>
+	);
+};
+
+const Head: React.FC<HeadModalProps> = ({ children, title, closeModal }) => {
+	return (
+		<div className={styles.modal__head}>
+			<button onClick={closeModal} className={styles.close__btn} title="Close modal">
+				<IoMdClose />
+			</button>
+
+			<h2 className={styles.modal__title}>{title}</h2>
+			{children}
+		</div>
+	);
+};
+
+const Footer: React.FC<FooterModalProps> = ({ children }) => {
+	return <div className={styles.modal__footer}>{children}</div>;
+};
+
+Modal.Head = Head;
+Modal.Footer = Footer;
+
+export default Modal;
