e521385e6a5b81529911f353fb9ba278f3392be8
NikVogri <55229210+NikVogri@users.noreply.github.com>
2022-02-08 21:57:27 +0100

    feat: public nav header

--- components/UI/Header/index.tsx
+++ components/UI/Header/index.tsx
@@ -1,5 +1,6 @@
 import UserNavSettings from "components/UserNavSettings";
 import useAuth from "hooks/useAuth";
+import Link from "next/link";
 import Notifications from "../NavNotifications";
 
 import styles from "./Header.module.scss";
@@ -9,10 +10,19 @@ const Header: React.FC = () => {
 
 	return (
 		<nav className={styles.header}>
-			{isAuth && (
+			{isAuth ? (
 				<div className={styles.header__container}>
 					<Notifications />
 					<UserNavSettings username={user?.username!} avatar={user?.avatar} />
+				</div>
+			) : (
+				<div className={styles.header__container}>
+					<Link href="/login">
+						<a className={`${styles.auth__btn} ${styles.auth__btn_login}`}>Login</a>
+					</Link>
+					<Link href="/register">
+						<a className={`${styles.auth__btn} ${styles.auth__btn_register}`}>Get started</a>
+					</Link>
 				</div>
 			)}
 		</nav>
