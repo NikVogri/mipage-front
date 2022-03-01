ccc9b80ed21413fc6971742859eb4b63470230e2
NikVogri <55229210+NikVogri@users.noreply.github.com>
2022-03-01 22:01:01 +0100

    feat: create comment logic & loading spinner

--- features/comment/commentApi.ts
+++ features/comment/commentApi.ts
@@ -1,5 +1,6 @@
 import baseApi from "features/baseApi";
 import { TodoItemComment } from "models";
+import { toast } from "react-toastify";
 
 export const commentsExtendedApi = baseApi.injectEndpoints({
 	endpoints: (build) => ({
@@ -8,7 +9,31 @@ export const commentsExtendedApi = baseApi.injectEndpoints({
 				url: `pages/${pageId}/todo-items/${todoItemId}/comments?size=10`,
 			}),
 		}),
+		createTodoItemComment: build.mutation<TodoItemComment, { pageId: string; todoItemId: string; body: string }>({
+			query: ({ pageId, body, todoItemId }) => ({
+				url: `pages/${pageId}/todo-items/${todoItemId}/comments`,
+				method: "POST",
+				body: { body },
+			}),
+			async onQueryStarted({ pageId, todoItemId }, { dispatch, queryFulfilled }) {
+				try {
+					const { data: newComment } = await queryFulfilled;
+
+					dispatch(
+						commentsExtendedApi.util.updateQueryData(
+							"getTodoItemComments",
+							{ pageId, todoItemId },
+							(comments) => {
+								comments.unshift(newComment);
+							}
+						)
+					);
+				} catch {
+					toast.error("Could not post comment, please try again later");
+				}
+			},
+		}),
 	}),
 });
 
-export const { useGetTodoItemCommentsQuery } = commentsExtendedApi;
+export const { useGetTodoItemCommentsQuery, useCreateTodoItemCommentMutation } = commentsExtendedApi;
