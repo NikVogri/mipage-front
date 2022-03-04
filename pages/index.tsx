c183002f8fe3f3f2768d998556534fb22ccf4ce9
NikVogri <55229210+NikVogri@users.noreply.github.com>
2022-03-04 21:21:44 +0100

    refactor: index page now indicates that Mipage is under construction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,39 @@
+import useAuth from "hooks/useAuth";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { MdAccountCircle, MdAppRegistration, MdBuildCircle, MdCreate, MdSupervisedUserCircle } from "react-icons/md";
+
+import styles from "../styles/pages/Index.module.scss";
 
 const Home: NextPage = () => {
+	const { isAuth } = useAuth();
 	return (
-		<div>
+		<div className={styles.container}>
 			<Head>
 				<title>Mipage | Your own digital notebook</title>
 			</Head>
 
-			<main>
-				<h1>hello</h1>
+			<main className={styles.under__construction__container}>
+				<MdBuildCircle size={84} />
+				<h1>Thank you for visiting Mipage</h1>
+				<h2>Mipage is currently under construction and some functionality might not work as expected.</h2>
+
+				{isAuth ? (
+					<Link href="/pages">
+						<a className={styles.register}>Visit your pages</a>
+					</Link>
+				) : (
+					<Link href="/register">
+						<a className={styles.register}>
+							<MdAccountCircle size={18} />
+							Create your account now!
+						</a>
+					</Link>
+				)}
+
+				<p>Created with {"❤️"} in Slovenia</p>
 			</main>
-			<Link href="/pages">
-				<a>pages</a>
-			</Link>
 		</div>
 	);
 };
