8b122c8c628c18ca2407408edfc4ecb76ea7f346
NikVogri <55229210+NikVogri@users.noreply.github.com>
2022-03-05 15:21:30 +0100

    feat: toggle todo item completion

--- components/Page/TodoListItem/index.tsx
+++ components/Page/TodoListItem/index.tsx
@@ -1,7 +1,15 @@
-import { useRemoveTodoItemMutation } from "features/todo/todoApi";
+import { useCompleteTodoItemMutation, useRemoveTodoItemMutation } from "features/todo/todoApi";
 import { truncate } from "helpers/truncateText";
 import { useState } from "react";
-import { MdCheckCircle, MdOpenInFull, MdOutlineRemoveCircle, MdRemove } from "react-icons/md";
+import { IoMdCloseCircle } from "react-icons/io";
+import {
+	MdCheckCircle,
+	MdCompress,
+	MdOpenInFull,
+	MdOutlineRemoveCircle,
+	MdRemove,
+	MdRemoveCircle,
+} from "react-icons/md";
 
 import styles from "./TodoListItem.module.scss";
 
@@ -15,12 +23,19 @@ interface TodoListItemProps {
 }
 
 const TodoListItem: React.FC<TodoListItemProps> = ({ completed, title, todoItemId, pageId, todoId, onOpenModal }) => {
-	const [removeTodoItem, { isLoading }] = useRemoveTodoItemMutation();
+	const [removeTodoItem, { isLoading: removeLoading }] = useRemoveTodoItemMutation();
+	const [toggleCompleteTodoItem, { isLoading: updateLoading }] = useCompleteTodoItemMutation();
 	const [showControls, setShowControls] = useState(false);
 
 	const handleDeleteTodoItem = async () => {
 		await removeTodoItem({ pageId, todoItemId, todoId });
 	};
+
+	const handleCompleteToggleTodoItem = async () => {
+		await toggleCompleteTodoItem({ pageId, todoItemId, todoId });
+	};
+
+	const isLoading = updateLoading || removeLoading;
 
 	return (
 		<li
@@ -34,9 +49,14 @@ const TodoListItem: React.FC<TodoListItemProps> = ({ completed, title, todoItemI
 			<div className={`${styles.todo__item__controls} ${showControls ? styles.active : ""}`}>
 				<hr />
 				<div className={styles.controls__btn__container}>
-					<button title="Complete" className={styles.complete} disabled={isLoading}>
-						<MdCheckCircle size={18} />
-						<span>Complete</span>
+					<button
+						title="Complete"
+						onClick={handleCompleteToggleTodoItem}
+						className={styles.complete}
+						disabled={isLoading}
+					>
+						{!completed ? <MdCheckCircle size={18} /> : <IoMdCloseCircle size={18} />}
+						<span>{!completed ? "Complete" : "Uncomplete"}</span>
 					</button>
 					<button
 						title="Delete"
