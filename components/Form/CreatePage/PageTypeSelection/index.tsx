484b604cc4ac1fbeebb530916e31e8822687725e
NikVogri <55229210+NikVogri@users.noreply.github.com>
2022-03-13 18:20:07 +0100

    refactor: disable notebook pages

--- components/Form/CreatePage/PageTypeSelection/index.tsx
+++ components/Form/CreatePage/PageTypeSelection/index.tsx
@@ -17,19 +17,16 @@ const PageTypeSelection: React.FC<PageTypeStepProps> = ({ setType, pageType }) =
 			<h2 className={styles.heading__section}>Select page type</h2>
 
 			<div className={styles.selection__container}>
-				<SelectCard
-					id="notebook"
-					isActive={pageType === PageType.notebook}
-					onSelect={() => setType(PageType.notebook)}
-				>
-					<h3 className={styles.title}>Notebook</h3>
-					<BsBook size={64} />
-					<p className={styles.description}>Your own accessible anywhere digital notebook</p>
-				</SelectCard>
 				<SelectCard id="todo" isActive={pageType === PageType.todo} onSelect={() => setType(PageType.todo)}>
 					<h3 className={styles.title}>Todo List</h3>
 					<BsCardChecklist size={64} />
 					<p className={styles.description}>Never forget anything, create multiple to-do lists</p>
+				</SelectCard>
+				<SelectCard id="notebook" disabled={true} isActive={pageType === PageType.notebook} onSelect={() => {}}>
+					<h3 className={styles.title}>Notebook</h3>
+					<BsBook size={64} />
+					<p className={styles.description}>Your own accessible anywhere digital notebook</p>
+					<h4>Currently not available</h4>
 				</SelectCard>
 			</div>
 		</>
--- components/Form/SelectCard/index.tsx
+++ components/Form/SelectCard/index.tsx
@@ -6,15 +6,16 @@ interface SelectCardProps {
 	children: any;
 	id: any;
 	isActive: boolean;
+	disabled?: boolean;
 	onSelect: (selection: any) => void;
 }
 
-const SelectCard: React.FC<SelectCardProps> = ({ children, id, isActive, onSelect }: SelectCardProps) => {
+const SelectCard: React.FC<SelectCardProps> = ({ children, id, isActive, onSelect, disabled }: SelectCardProps) => {
 	const handleClick = () => onSelect(id);
 
 	return (
 		<button
-			className={`${styles.select__card} ${isActive ? styles.active : ""}`}
+			className={`${styles.select__card} ${disabled ? styles.disabled : ""} ${isActive ? styles.active : ""}`}
 			type="button"
 			onClick={handleClick}
 		>
