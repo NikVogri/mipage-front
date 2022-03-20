7a57d9ab1e47e6ac7985f21f5fd7891371cdd0fd
NikVogri <55229210+NikVogri@users.noreply.github.com>
2022-03-20 16:24:29 +0100

    feat: page delete and update endpoints support

--- features/baseApi.ts
+++ features/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BE_BASE_URL, credentials: "include" }),
 	endpoints: () => ({}),
-	tagTypes: ["SidebarPages", "UserPages"],
+	tagTypes: ["SidebarPages", "UserPages", "Page"],
 });
 
 export default baseApi;
