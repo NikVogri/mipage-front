ab93175930b0a0d84d311f6257a8b560e43cc15e
NikVogri <vogrinec.nik@gmail.com>
2022-05-02 16:42:47 +0200

    feat: page member list components

---
+++ components/MembersList/MembersList.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { PageMember, PageOwner } from "models";
+
+import MembersListItem from "components/MembersListItem/MembersListItem";
+
+import styles from "./MembersList.module.scss";
+
+interface MembersListProps {
+	users: PageMember[];
+	owner: PageOwner;
+	pageId: string;
+}
+
+const MembersList: React.FC<MembersListProps> = ({ owner, users, pageId }) => {
+	console.log(users);
+
+	if (users.length > 0) {
+		return (
+			<div className={styles.members__container}>
+				<ul>
+					{users.map((u: PageMember) => (
+						<MembersListItem
+							key={u.id}
+							pageId={pageId}
+							ownerId={owner.id}
+							avatar={u.avatar}
+							email={u.email}
+							id={u.id}
+							username={u.username}
+						/>
+					))}
+				</ul>
+			</div>
+		);
+	}
+
+	return (
+		<div className={styles.members__container}>
+			<span className={styles.no__members}>No members yet</span>
+		</div>
+	);
+};
+
+export default MembersList;
--- models.ts
+++ models.ts
@@ -1,7 +1,7 @@
 export interface User {
 	id: string;
 	username: string;
-	email?: string;
+	email: string;
 	avatar: string | null;
 	createdAt?: Date;
 	updatedAt?: Date;
