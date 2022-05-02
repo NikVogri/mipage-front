6064145c52f7b15ea987c9fdb341e0cfa37c9b34
NikVogri <vogrinec.nik@gmail.com>
2022-05-02 16:46:59 +0200

    refactor: use new components to add users and display page members list

--- components/Page/PageMembers/index.tsx
+++ components/Page/PageMembers/index.tsx
@@ -1,175 +1,34 @@
+import Avatar from "components/Avatar";
+import { useState } from "react";
 import { PageMember, PageOwner } from "models";
-import { ChangeEvent, Dispatch, SetStateAction, useCallback, useMemo, useState } from "react";
-import { DebounceInput } from "react-debounce-input";
-import { useAddMemberToPageMutation, useRemoveMemberFromPageMutation } from "features/member/memberApi";
-import { useLazyGetUsersQuery } from "features/user/userApi";
 import { HiDotsHorizontal } from "react-icons/hi";
 
-import LoadingWrapper from "components/UI/LoadingWrapper";
-import Modal from "components/UI/Modal";
-import Avatar from "components/Avatar";
+import MembersModal from "components/UI/modals/MembersModal/MembersModal";
 
 import styles from "./PageMembers.module.scss";
-import useAuth from "hooks/useAuth";
 
 interface PageMembersProps {
 	owner: PageOwner;
 	members: PageMember[];
 	pageId: string;
 }
-interface AddMembersModalProps {
-	isOpen: boolean;
-	pageId: string;
-	members: PageMember[];
-	owner: PageOwner;
-	setIsOpen: Dispatch<SetStateAction<boolean>>;
-	setIsClosed: () => void;
-}
-
-const AddMembersModal: React.FC<AddMembersModalProps> = ({
-	isOpen,
-	setIsOpen,
-	setIsClosed,
-	pageId,
-	members,
-	owner,
-}) => {
-	const [fetchUserList, { data, isFetching: isFetchingUserList }] = useLazyGetUsersQuery();
-	let [addMemberToPage, { isLoading: isLoadingAddMember }] = useAddMemberToPageMutation();
-	let [removeMemberFromPage, { isLoading: isLoadingRemoveMember }] = useRemoveMemberFromPageMutation();
-
-	const [toInviteUserEmail, setToInviteUserEmail] = useState("");
-	const [toRemoveUser, setToRemoveUser] = useState("");
-
-	const handleSearchUser = async (e: ChangeEvent<HTMLInputElement>) => {
-		if (!e.target.value.length) return;
-		await fetchUserList({ query: e.target.value });
-	};
-
-	const handleAddUserToPage = async (email: string) => {
-		setToInviteUserEmail(email);
-		await addMemberToPage({ email, pageId });
-		setToInviteUserEmail("");
-	};
-
-	const handleRemoveUserFromPage = async (id: string) => {
-		setToRemoveUser(id);
-		await removeMemberFromPage({ id, pageId });
-		setToRemoveUser("");
-	};
-
-	const memberOrOwner = useCallback(
-		(id: string) => members.some((m: PageMember) => m.id === id) || owner?.id === id,
-		[members, owner.id]
-	);
-
-	const users = useMemo(() => (data ? data : [owner, ...members]), [data, members, owner]);
-
-	return (
-		<Modal isOpen={isOpen} setIsOpen={setIsOpen} contentLabel="Manage page members">
-			<Modal.Head title="Manage page members" closeModal={setIsClosed} />
-			<form className={styles.search__for_users__form}>
-				<div className="form-group">
-					<label className="label" htmlFor="search-for-users">
-						Search for users by their username
-					</label>
-					<DebounceInput
-						id="search-for-users"
-						minLength={2}
-						debounceTimeout={500}
-						onChange={handleSearchUser}
-						className="form-control form-control-modal"
-					/>
-				</div>
-			</form>
-
-			{isFetchingUserList && (
-				<LoadingWrapper isLoading={true} size={24} className={styles.loader} delay={0}>
-					<span></span>
-				</LoadingWrapper>
-			)}
-			{!isFetchingUserList && (
-				<div className={styles.user__results}>
-					{!users.length && <span className={styles.user__results__no__found}>No users found</span>}
-					{users.length > 0 && (
-						<ul>
-							{users.map((u) => (
-								<li className={styles.user__item} key={u.id}>
-									<div className={styles.results__username__container}>
-										<Avatar username={u.username} avatar={u.avatar} size="md" />
-										<span className={styles.results__username}>{u.username}</span>
-									</div>
-									<span>{u.email}</span>
-									{memberOrOwner(u.id) ? (
-										owner.id === u.id ? (
-											<button
-												title="Remove from page"
-												className={styles.owner__btn}
-												disabled={true}
-											>
-												Owner
-											</button>
-										) : (
-											<LoadingWrapper
-												isLoading={isLoadingRemoveMember && u.id === toRemoveUser}
-												size={16}
-												delay={500}
-												className={styles.loader__invite}
-											>
-												<button
-													title="Remove from page"
-													onClick={() => handleRemoveUserFromPage(u.id)}
-													className={styles.kick__btn}
-													disabled={isLoadingRemoveMember}
-												>
-													Kick
-												</button>
-											</LoadingWrapper>
-										)
-									) : (
-										<LoadingWrapper
-											isLoading={isLoadingAddMember && u.email === toInviteUserEmail}
-											size={16}
-											delay={500}
-											className={styles.loader__invite}
-										>
-											<button
-												title="Add to page"
-												onClick={() => handleAddUserToPage(u.email)}
-												disabled={isLoadingAddMember}
-											>
-												Add
-											</button>
-										</LoadingWrapper>
-									)}
-								</li>
-							))}
-						</ul>
-					)}
-				</div>
-			)}
-		</Modal>
-	);
-};
 
 export const PageMembers: React.FC<PageMembersProps> = ({ owner, members, pageId }) => {
-	const [showInviteMembersModal, setShowInviteMembersModal] = useState(false);
-	const { user } = useAuth();
+	const [showMembersModal, setShowMembersModal] = useState(false);
 
 	return (
 		<>
 			<div className={`${styles.page__members}`}>
 				<div className={styles.card_head_container}>
 					<h3>Members ({members.length + 1})</h3>
-					{owner.id === user?.id && (
-						<div className={styles.button_container}>
-							<button title="Add new members" onClick={() => setShowInviteMembersModal(true)}>
-								<HiDotsHorizontal size={16} />
-							</button>
-						</div>
-					)}
+					<div className={styles.button_container}>
+						<button onClick={() => setShowMembersModal(true)}>
+							<HiDotsHorizontal size={16} />
+						</button>
+					</div>
 				</div>
 
+				{/* TODO: make this look better */}
 				<ul className={styles.members__container}>
 					<li style={{ transform: `translateX(-${0 * 20}px)`, zIndex: `30` }}>
 						<Avatar
@@ -197,11 +56,11 @@ export const PageMembers: React.FC<PageMembersProps> = ({ owner, members, pageId
 					)}
 				</ul>
 			</div>
-			<AddMembersModal
+			<MembersModal
 				owner={owner}
-				isOpen={showInviteMembersModal}
-				setIsClosed={() => setShowInviteMembersModal(false)}
-				setIsOpen={setShowInviteMembersModal}
+				isOpen={showMembersModal}
+				setIsClosed={() => setShowMembersModal(false)}
+				setIsOpen={setShowMembersModal}
 				pageId={pageId}
 				members={members}
 			/>
