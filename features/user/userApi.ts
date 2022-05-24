ef6614f4c0b88b9a4a80bf8e245125567cfb643d
NikVogri <vogrinec.nik@gmail.com>
2022-05-24 22:31:56 +0200

    chore: remove deprecated endpoint call

--- features/user/userApi.ts
+++ features/user/userApi.ts
@@ -3,13 +3,7 @@ import baseApi from "features/baseApi";
 import { SearchedUser } from "models";
 
 export const userExtendedApi = baseApi.injectEndpoints({
-	endpoints: (build) => ({
-		getUsers: build.query<SearchedUser[], { query: string }>({
-			query: ({ query }) => ({
-				url: `users/find?q=${query}`,
-			}),
-		}),
-	}),
+	endpoints: (build) => ({}),
 });
 
-export const { useLazyGetUsersQuery } = userExtendedApi;
+export const {} = userExtendedApi;
