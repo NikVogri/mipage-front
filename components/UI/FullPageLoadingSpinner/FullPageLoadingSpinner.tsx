1d5566f56bc21993c12b504611ee1aa59182f755
NikVogri <vogrinec.nik@gmail.com>
2022-11-02 20:02:27 +0100

    chore: cleaner auth loading

--- components/UI/FullPageLoadingSpinner/FullPageLoadingSpinner.tsx
+++ components/UI/FullPageLoadingSpinner/FullPageLoadingSpinner.tsx
@@ -3,13 +3,18 @@ import React, { useEffect, useState } from "react";
 
 import styles from "./FullPageLoadingSpinner.module.scss";
 
-interface FullPageLoadingSpinnerProps {}
-const FullPageLoadingSpinner: React.FC<FullPageLoadingSpinnerProps> = ({}) => {
+interface FullPageLoadingSpinnerProps {
+	isLoading: boolean;
+	delay?: number;
+}
+
+const FullPageLoadingSpinner: React.FC<FullPageLoadingSpinnerProps> = ({ isLoading, delay }) => {
 	const [tipIndex, setTipIndex] = useState(0);
+	const [loading, setLoading] = useState(false);
 
 	const tips = [
 		"Use todo pages to create todo lists!",
-		"Public pages are available to everyone!",
+		"Public pages can be accessed by anyone!",
 		"Add people to edit your pages!",
 	];
 
@@ -27,9 +32,24 @@ const FullPageLoadingSpinner: React.FC<FullPageLoadingSpinnerProps> = ({}) => {
 		return () => clearInterval(interval);
 	}, [tips.length]);
 
+	useEffect(() => {
+		let loader: NodeJS.Timeout;
+		if (isLoading) {
+			loader = setTimeout(() => setLoading(true), typeof delay === "number" ? delay : 350);
+		} else {
+			setLoading(false);
+		}
+
+		return () => clearTimeout(loader);
+	}, [isLoading, delay]);
+
+	if (!loading) {
+		return <></>;
+	}
+
 	return (
 		<div className={styles.background}>
-			<LoadingSpinner size={46} />
+			<LoadingSpinner size={32} />
 			<h3>{tips[tipIndex]}</h3>
 		</div>
 	);
