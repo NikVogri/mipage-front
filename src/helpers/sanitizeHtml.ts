dc3dc60946b4a725007d8a97cb02377ce504b908
NikVogri <vogrinec.nik@gmail.com>
2022-11-20 20:49:48 +0100

    feat: wysiwyg todo item description

--- src/helpers/sanitizeHtml.ts
+++ src/helpers/sanitizeHtml.ts
@@ -1,4 +1,4 @@
-export const SAFE_HTML_TAGS = ["h1", "h2", "h3", "h4", "h5", "h6", "ol", "ul", "li", "strong", "u", "i", "code"];
+export const SAFE_HTML_TAGS = ["h1", "h2", "h3", "h4", "h5", "h6", "ol", "ul", "li", "strong", "u", "i", "code", "br"];
 
 import DOMPurify from "dompurify";
 
