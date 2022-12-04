c3b96414014be9537eab4d66358705fa8738f368
NikVogri <vogrinec.nik@gmail.com>
2022-12-04 19:23:37 +0100

    chore: move multiple related functions into one file

--- src/helpers/editorContentToRawString.ts
+++
@@ -1,6 +0,0 @@
-import { convertToRaw, EditorState } from "draft-js";
-
-export const editorContentToRawString = (editorState: EditorState): string => {
-	const rawContent = convertToRaw(editorState.getCurrentContent());
-	return JSON.stringify(rawContent);
-};
