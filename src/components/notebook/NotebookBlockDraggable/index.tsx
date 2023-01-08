207b98fb4e0b35c9ad236ffdb8cdef5fcfd856e5
NikVogri <vogrinec.nik@gmail.com>
2023-01-08 19:57:06 +0100

    feat: NotebookBlockDraggable component

---
+++ src/components/notebook/NotebookBlockDraggable/index.tsx
@@ -0,0 +1,47 @@
+import { DragEvent, ReactElement, useRef, useState } from "react";
+import { MdDragHandle } from "react-icons/md";
+
+import styles from "./NotebookBlockDraggable.module.scss";
+
+interface NotebookBlockDraggableProps {
+	children: ReactElement;
+	notebookBlockId: string;
+}
+
+const NotebookBlockDraggable: React.FC<NotebookBlockDraggableProps> = ({ notebookBlockId, children }) => {
+	const [isDragged, setIsDragged] = useState(false);
+	const draggableRef = useRef<HTMLDivElement>(null);
+
+	const handleDragStart = (e: DragEvent<HTMLDivElement>) => {
+		e.dataTransfer.setData("notebookBlockId", notebookBlockId);
+		e.dataTransfer.setDragImage(draggableRef.current!, 0, 0);
+
+		if (!isDragged) {
+			setIsDragged(true);
+		}
+	};
+
+	const handleDragEnd = (e: DragEvent<HTMLDivElement>) => {
+		e.preventDefault();
+
+		if (isDragged) {
+			setIsDragged(false);
+		}
+	};
+
+	return (
+		<div className={`${styles.draggable} ${isDragged ? styles.draggable__active : ""}`} ref={draggableRef}>
+			<div
+				className={styles.draggable__icon__container}
+				draggable
+				onDragStart={handleDragStart}
+				onDragEnd={handleDragEnd}
+			>
+				<MdDragHandle className={styles.draggable__icon} size={24} />
+			</div>
+			<div className={styles.content}>{children}</div>
+		</div>
+	);
+};
+
+export default NotebookBlockDraggable;
