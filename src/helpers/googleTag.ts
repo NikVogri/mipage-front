0248dd97705101625f5bd7367b5ce6a4aeba81ae
NikVogri <vogrinec.nik@gmail.com>
2023-04-23 23:34:55 +0200

    chore: remove gtag

--- src/helpers/googleTag.ts
+++
@@ -1,9 +0,0 @@
-export const logPageViewToGTag = (url: string) => {
-	(window as any).gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-		page_path: url,
-	});
-};
-
-export const logEventToGTag = ({ action, params }: { action: string; params: any }) => {
-	(window as any).gtag("event", action, params);
-};
