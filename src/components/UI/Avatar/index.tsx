21584091117d1090b903c44e2b5a787d52b51d3c
NikVogri <vogrinec.nik@gmail.com>
2023-05-14 21:05:09 +0200

    refactor: Avatar component

--- src/components/UI/Avatar/index.tsx
+++ src/components/UI/Avatar/index.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { getRgbaFromChar } from "helpers/color";
 
 import styles from "./Avatar.module.scss";
 
@@ -9,61 +9,28 @@ export interface AvatarProps {
 	size?: "md" | "lg" | "sm";
 	tooltip?: boolean;
 }
-export interface AvatarAdditionalProps {
-	additionalCount: number;
-	size?: "md";
-	outline?: string;
-}
 
-function getRandomColor(string: string): string {
-	const firstAlphabet = string.charAt(0).toLowerCase();
-	const asciiCode = firstAlphabet.charCodeAt(0);
-	const colorNum = asciiCode.toString() + asciiCode.toString() + asciiCode.toString();
-
-	var num = Math.round(0xffffff * parseInt(colorNum));
-	var r = (num >> 16) & 255;
-	var g = (num >> 8) & 255;
-	var b = num & 255;
-
-	return `rgb(${r}, ${g}, ${b}, 0.5)`;
-}
-
-const Avatar = ({ avatar, username, outline, size, tooltip = true }: AvatarProps) => {
-	if (!avatar) {
-		return (
-			<div
-				className={`${styles.avatar} ${size ? styles[size] : ""}`}
-				style={{
-					backgroundColor: getRandomColor(username),
-					border: outline ? `solid 2px ${outline}` : undefined,
-				}}
-			>
-				<span>{username[0].toUpperCase()}</span>
-
-				{tooltip && <div className={styles.additional__info}>{username}</div>}
-			</div>
-		);
-	}
-
+const Avatar = ({ username, outline, size, tooltip = true }: AvatarProps) => {
 	return (
 		<div
 			className={`${styles.avatar} ${size ? styles[size] : ""}`}
 			style={{
 				border: outline ? `solid 2px ${outline}` : undefined,
+				backgroundColor: getRgbaFromChar(username, 0.5),
 			}}
 		>
-			<Image
-				unoptimized // TODO: remove this line
-				src={avatar}
-				alt={username}
-				className={`${styles.avatar} ${styles.image}`}
-				width="100%"
-				height="100%"
-			/>
+			<span>{username[0]?.toUpperCase()}</span>
+
 			{tooltip && <div className={styles.additional__info}>{username}</div>}
 		</div>
 	);
 };
+
+export interface AvatarAdditionalProps {
+	additionalCount: number;
+	size?: "md";
+	outline?: string;
+}
 
 const Additional: React.FC<AvatarAdditionalProps> = ({ additionalCount, outline, size }) => {
 	return (
